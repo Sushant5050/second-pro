4d0fd34e05360362be6594bf96f16bf11b42ef0c
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-11-29 19:30:03 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second_pro
+{
+    internal class Program
+    {
+        public void square()
+        {
+            int a = 10, b;
+            b = a * a;
+            Console.WriteLine("square="+ b);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.square();
+            Console.ReadLine();
+        }
+    }
+}
